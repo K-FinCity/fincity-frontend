40a2682329a5a7cb657db846785b6cb055764705
Moisés García <mgarciaj1802@alumno.ipn.mx>
2024-06-23 21:45:39 -0600

    Add: Created the main file for the registration of teacher users

---
+++ pages/registro.jsx
@@ -0,0 +1,10 @@
+import { Chakra_Petch } from 'next/font/google';
+
+const chakra = Chakra_Petch({
+  subsets: ['latin'],
+  weight: ['300', '400', '600'],
+});
+
+export default function Page() {
+  return <></>;
+}
