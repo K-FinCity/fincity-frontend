f222312ec1bb729586c5109397adeb977d97ff45
Moisés García <mgarciaj1802@alumno.ipn.mx>
2024-06-25 22:30:20 -0600

    Revert "Fix: classes and reset"
    This reverts commit 10365ea1096f93133741b43f6d3c8ba409c257ef.

--- pages/registro.jsx
+++ pages/registro.jsx
@@ -16,7 +16,6 @@ export default function Page() {
     watch,
     formState: { errors },
     handleSubmit,
-    reset,
   } = useForm();
 
   const onSubmit = async (data) => {
@@ -49,9 +48,11 @@ export default function Page() {
       <div className="relative">
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          width="430"
+          height="171"
           viewBox="0 0 430 171"
           fill="none"
-          className="absolute lg:hidden"
+          className="absolute"
         >
           <path
             d="M0 0H430V137.453C399.406 125.945 366.017 124.112 334.347 132.2L282.5 145.441L180.098 168.042C161.738 172.094 142.609 170.888 124.903 164.563L0 119.943V0Z"
@@ -61,40 +62,15 @@ export default function Page() {
         </svg>
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          width="430"
+          height="179"
           viewBox="0 0 430 179"
           fill="none"
-          className="lg:hidden"
         >
           <path
             d="M0 0H430V156.496H414.792C392.784 156.496 370.88 153.484 349.689 147.545C323.879 140.31 296.794 138.839 270.352 143.236L61.0452 178.037C32.8884 182.719 6.01818 164.462 0 136.56V0Z"
             fill="#8B3FDE"
             fill-opacity="0.5"
-          />
-        </svg>
-
-        {/* DESKTOP */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 428 832"
-          fill="none"
-          className="hidden h-full lg:block"
-        >
-          <path
-            d="M428 0L428 832L53.7185 832V803.503C53.7185 760.404 60.9219 717.608 75.0311 676.884C92.2891 627.072 95.7964 573.527 85.1837 521.889L2.94284 121.73C-8.41144 66.4834 28.1437 12.7828 83.7054 3.08729L101.398 0L428 0Z"
-            fill="#682BA4"
-            fill-opacity="0.5"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 504 832"
-          fill="none"
-          className="hidden lg:block lg:absolute top-0 h-full"
-        >
-          <path
-            d="M504 0L504 832L100.107 832C132.969 774.469 138.14 705.189 114.183 643.417L76.635 546.605L5.82239 335.34C-2.99365 309.038 -0.488037 280.256 12.7404 255.873L151.558 0L504 0Z"
-            fill="#8B3FDE"
-            fill-opacity="0.7"
           />
         </svg>
       </div>
