a5f3eeb317766d645a06725cde94c1ac6affeb9c
Moisés García <mgarciaj1802@alumno.ipn.mx>
2024-07-01 21:11:28 -0600

    feat: Added authentication with credentials and JWT token saving in the session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,81 +1,102 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
-import API from '@/services/API';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import API from '@/services/API';
 
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      profile: (profile) => {
+        return {
+          id: profile.sub,
+          email: profile.email,
+          first_name: profile.given_name,
+          last_name: profile.family_name,
+          googleId: profile.sub,
+        };
+      },
     }),
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
-        email: { label: 'email', type: 'email', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
+        email: {
+          type: 'email',
+        },
+        password: {
+          type: 'password',
+        },
       },
-      async authorize() {
-        return true;
+      async authorize(credentials) {
+        try {
+          const { email, password } = credentials;
+
+          const user = await API.authenticateUser({ email, password });
+
+          if (user) {
+            return { id: user._id, token: user.token };
+          } else {
+            return null;
+          }
+        } catch (error) {
+          console.error('Error during authentication:', error);
+          return null;
+        }
       },
     }),
   ],
-  session: {
-    strategy: 'jwt',
-    maxAge: 432000, //5days
-  },
   pages: {
-    signIn: '/login',
-    signOut: '/',
+    /*signIn: '/login',
+    signOut: '/',*/
     error: '/registro',
   },
   callbacks: {
     async jwt({ token, user }) {
-      if (user) token.user = user;
+      if (user) {
+        token.accessToken = user.token;
+      }
       return token;
     },
     async session({ session, token }) {
-      session.user = token.user;
+      if (session) {
+        session.accessToken = token.accessToken;
+        console.log(session.accessToken);
+      }
       return session;
     },
     async signIn({ profile, credentials }) {
       if (credentials) {
-        /// REMOVE
-        // Lógica de petición a la BD en búsquieda del usuario con match en email y password
-        // PUEDE SER: const userFound = await find or email and password match... (<- back)
-        const user = {
-          password: 'gr3at@3wdsG',
-          email: 'moi@prueba.com',
-        };
-
-        if (
-          credentials?.email === user.email &&
-          credentials?.password === user.password
-        ) {
-          console.log(user);
-          return user;
-        } else {
-          return null;
-        }
-        /// REMOVE
-      } else {
+        return true;
+      } else if (profile) {
         try {
-          const data = {
-            user: null,
-            first_name: profile.given_name.split(' ')[0],
-            last_name: profile.family_name.split(' ')[0],
-            email: profile.email,
-            role: '6676ee3c23f3b664bbf5f50d',
-            password: 'gr3at@3wdsGz',
-          };
+          const user = await API.findUserByGoogleId(profile.id);
 
-          await API.createNewUser(data);
+          if (!user) {
+            const data = {
+              first_name: profile.given_name,
+              last_name: profile.family_name,
+              email: profile.email,
+              // Profesor role
+              role: '6676ee3c23f3b664bbf5f50d',
+              googleId: profile.id,
+              googleToken: profile.access_token,
+              password: 'gr3at@3wdsGz',
+            };
+
+            await API.createNewUser(data);
+          } else {
+            await API.updateUserGoogleInfo(profile.id, profile.access_token);
+          }
 
           return true;
         } catch (e) {
-          console.error(e);
+          console.error('Error handling Google sign-in:', e);
+          return false;
         }
       }
+      return false;
     },
   },
+  secret: process.env.NEXTAUTH_SECRET,
 });
--- services/API/index.jsx
+++ services/API/index.jsx
@@ -1,17 +1,61 @@
-const BASE_URI = 'http://localhost:3002';
+const BASE_URI = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-const api = {
-  createNewUser: async (user) => {
-    const response = await fetch(`${BASE_URI}/users/`, {
+const API = {
+  async authenticateUser({ email, password }) {
+    const r = await fetch(`${BASE_URI}/users/login`, {
       method: 'POST',
-      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const response = await r.json();
+
+    if (!response.data) throw new Error('Authentication failed');
+
+    return await response.data;
+  },
+
+  async createNewUser(data) {
+    const response = await fetch(`${BASE_URI}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) throw new Error('Failed to create user');
+
+    return await response.json();
+  },
+
+  findUserByGoogleId: async (googleId) => {
+    const response = await fetch(`${API_URL}/users/auth/google/${googleId}`, {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    const data = await response.json();
-    return data;
+    if (!response.ok) throw new Error('User not found');
+    return response.json();
+  },
+
+  updateUserGoogleInfo: async (data, googleId) => {
+    const response = await fetch(
+      `${API_URL}/users/auth/google/update/${googleId}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    if (!response.ok) throw new Error('Update failed');
+    return response.json();
   },
 };
 
-export default api;
+export default API;
